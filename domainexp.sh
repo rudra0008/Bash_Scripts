d66a1086798b0435c5fe5147b82570d59f8eb9eb
rudra0008 <rudra215@gmail.com>
2017-10-18 20:48:06 +0530

    modified for different patterns

--- domainexp.sh
+++ domainexp.sh
@@ -2,3 +2,4 @@
 echo "Enter Domain name:"
 read domain
 whois $domain | awk '/Registry Expiry Date:/ {print "Expiry Date: " $4}'
+whois $domain | awk '/Expiration/ {print "Expiry Date: " $2" "$3" "$4}'
