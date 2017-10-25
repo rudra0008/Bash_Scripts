016efc771eafdc74cf42f7eda4ec68fc1c2a41fa
rudra0008 <rudra215@gmail.com>
2017-10-25 11:36:18 +0530

    getopt with 1 option and 1 argument

---
+++ getopts1-1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ $# -eq 0 ];
+then
+      echo "USAGE: ./getopts1-1.sh <option> <argument>";
+      echo "Option: -n , Argument: any name.";
+       exit 0
+else 
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      echo "name is $OPTARG";
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument.";
+      exit 1
+      ;;
+   \?)
+      echo "USAGE: ./getopts1-1.sh <option> <argument>";
+      echo "Option: -n , Argument: any name.";
+      exit 1
+     ;;
+    *)
+      echo "Invalid option: -$OPTARG";
+      echo "USAGE: ./getopts1-1.sh <option> <argument>";
+      echo "Option: -n , Argument: any name.";
+      exit 1
+      ;;
+  esac
+done
+fi
